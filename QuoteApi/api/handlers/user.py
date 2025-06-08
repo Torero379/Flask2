28c1264baa64eefb5b02fb3237ef6b9da63abe98
Torero379 <i.kipelov@mail.ru>
2025-06-08 12:37:26 +0300

    error post user

--- QuoteApi/api/handlers/user.py
+++ QuoteApi/api/handlers/user.py
@@ -1,14 +1,25 @@
+from flask import jsonify, request
 from api import app, db
 from api.models.user import UserModel
+from api.schemas.user import UserSchema, user_schema
 
 
+@app.get('/users/<int:user_id>')
 def get_user_by_id(user_id: int):
-    pass
-
-
+    user = db.get_or_404(UserModel, user_id, description=f"User with id={user_id} not found")
+    return jsonify(user_schema.dump(user)), 200
+    
+ 
+@app.get('/users')
 def get_users():
-    pass
+    users = db.session.scalars(db.select(UserModel)).all()
+    return jsonify(user_schema.dump(users, many=True)), 200
 
+
+@app.post("/user")
 def create_user():
-    pass
+    user = UserSchema.loads(request.data)  
+    db.session.add(user)
+    db.session.commit()
+    return jsonify(user_schema.dump(user)), 201
 
